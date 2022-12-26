dce6d689cb3ddcf554eae267868f839fae16e150
Daniel Lee <danlee@hey.com>
2022-12-26 16:26:19 -0700

    start gin web server

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+import "net/http"
 
 func main() {
-	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
